9a358b9a34a0eb46bf834cbbddc17c623095fc31
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-18 19:35:18 +0200

    Start new exercise

---
+++ exercises/exercise_248.py
@@ -0,0 +1,8 @@
+"""
+The first number n for which φ(n)=13! is 6227180929.
+
+Find the 150,000th such number.
+
+φ(n) function documenation:
+https://en.wikipedia.org/wiki/Euler%27s_totient_function
+"""
