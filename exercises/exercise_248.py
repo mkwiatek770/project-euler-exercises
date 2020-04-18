168f766949c49494d54938590e5aeff5efcb271a
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-18 19:38:34 +0200

    Greatest common divisor function

--- exercises/exercise_248.py
+++ exercises/exercise_248.py
@@ -6,3 +6,22 @@ Find the 150,000th such number.
 φ(n) function documenation:
 https://en.wikipedia.org/wiki/Euler%27s_totient_function
 """
+
+def gcd(a: int, b: int):
+    if a > b:
+        bigger, smaller = a, b
+    else:
+        bigger, smaller = a, b
+    while True:
+        reminder = bigger % smaller
+        if reminder == 0:
+            return smaller
+        bigger = smaller
+        smaller = reminder
+
+if __name__ == "__main__":
+    print(gcd(34, 17))
+    print(gcd(45, 10))
+    print(gcd(10, 45))
+
+
