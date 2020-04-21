40730e125fde6f4cbb9a1a614ad816ccbd50077c
Michał Kwiatek <michalkwiatek9@o2.pl>
2020-04-21 22:40:57 +0200

    Refactor code

--- exercises/exercise_17.py
+++ exercises/exercise_17.py
@@ -1,62 +1,55 @@
 
 DIGIT_MAP = {
-            1: ("one", 3),
-            2: ("two", 3),
-            3: ("three", 5),
-            4: ("four", 4),
-            5: ("five", 4),
-            6: ("six", 3),
-            7: ("seven", 5),
-            8: ("eight", 5),
-            9: ("nine", 4),
-            10: ("ten", 3),
-            11: ("eleven", 6),
-            12: ("twelve", 6),
-            20: ("twenty", 6),
-            30: ("thirty", 6),
-            40: ("fourty", 6),
-            50: ("fifty", 5),
-            60: ("sixty", 5),
-            70: ("seventy", 7),
-            80: ("eighty", 6),
-            90: ("ninety", 6),
-            100: ("hundred", 7),
-            1000: ("thousand", 8),
-        }
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    20: "twenty",
+    30: "thirty",
+    40: "fourty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+    100: "hundred",
+    1000: "thousand",
+}
 
 
 def letter_used(num):
-    counter = 0
     full_phrase = []
+    counter = 0
 
     if num // 1000 >= 1:
-        thousands = DIGIT_MAP[num // 1000]
-        thousand = DIGIT_MAP[1000]
-        
-        full_phrase.extend([thousands[0], thousand[0]])
-        counter += thousands[1] + thousand[1]
-
+        full_phrase.extend([DIGIT_MAP[num // 1000], DIGIT_MAP[1000]])
         num = num - (num // 1000)*1000
 
     if num // 100 >= 1:
-        hundreds = num // 100
-        counter += DIGIT_MAP[hundreds][1]
-        counter += DIGIT_MAP[100][1]
+        full_phrase.extend([DIGIT_MAP[num // 100], DIGIT_MAP[100]])
         num = num - (num // 100)*100
         if num > 0:
-            counter += len("and")
+            full_phrase.append("and")
     if num // 10 >= 1:
         if num in DIGIT_MAP:
-            counter += DIGIT_MAP[num][1]
+            full_phrase.append(DIGIT_MAP[num])
             num = 0
         else:
-            decimal = num // 10
-            counter += DIGIT_MAP[int(str(decimal) + '0')][1]
+            full_phrase.append(DIGIT_MAP[int(f"{num // 10}0")])
             num = num - (num // 10)*10
     if num > 0:
-        counter += DIGIT_MAP[num][1]
+        full_phrase.append(DIGIT_MAP[num])
     print(full_phrase)
-    return counter
+    return len("".join(full_phrase))
+
 
 def letter_used_v2(number):
     total = sum(DIGIT_MAP[int(d)][1] for d in str(number))
@@ -75,11 +68,10 @@ if __name__ == "__main__":
     print(letter_used(1000))
     print(letter_used(100))
 
-    #counter = 0
-    #for i in range(1, 1001):
+    # counter = 0
+    # for i in range(1, 1001):
     #    counter += letter_used(i)
-    #print(counter)
+    # print(counter)
 
     # Version 2
     print(letter_used_v2(342))
-
